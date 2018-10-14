3e529b22146e15bfe4135d6175f5c8963efd03f2
Panha <sopanhakok@gmail.com>
2018-10-14 18:33:06 +0700

    full sticky-nav

--- javascript/app.js
+++ javascript/app.js
@@ -21,7 +21,16 @@ $('.hamburger').click(function() {
 $('#head').sticky();
 $('#head').on('sticky-start', function(){
   $('#head').css("background-color", "#242424");
+  $('#ul').css("margin-top", "55px");
+  $('#logo').css("margin-top", "13px");
+  $('#logo').css("margin-bottom", "13px");
+  $('nav a').css("padding-bottom", "34px");
 });
 $('#head').on('sticky-end', function(){
   $('#head').css("background-color", "transparent");
+  $('#head').css('display',"block");
+  $('#ul').css("margin-top", "75px");
+  $('#logo').css("margin-top", "40px");
+  $('#logo').css("margin-bottom", "0px");
+  $('nav a').css("padding-bottom", "25px");
 });
